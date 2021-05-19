d90f7d6bab6177cf4aea4aef54cb042d785903d1
Supritam <supritamsadhukhan@gmail.com>
2021-05-19 18:25:48 +0000

    Environemt changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  R_BASE_URL: 'http://localhost:8080',
+  R_BASE_URL: 'https://cowin-slot-313213.el.r.appspot.com',
   R_SUBSCRIBE_USER: '/v1/public/subscribe',
-  R_UNSUBSCRIBE_USER: '/v1/public/unsubscribe/',
+  R_UNSUBSCRIBE_USER: '/v1/public/unsubscribe',
 
   COWIN_BASE_URL: 'https://cdn-api.co-vin.in/api',
   COWIN_DISTRICT: '/v2/admin/location/districts/',
