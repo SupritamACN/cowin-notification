2396b565849f1c407f93d17c0fcff4bd62cc9e82
Supritam Sadhukhan <sadhukhan.supritam-extern@renault.com>
2021-05-20 20:49:46 +0530

    correcting url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   R_BASE_URL: 'https://cowin-slot-313213.el.r.appspot.com',
   R_SUBSCRIBE_USER: '/v1/public/subscribe/',
   R_UNSUBSCRIBE_USER: '/v1/public/unsubscribe/',
-  TELEGRAM_SUBSCRIBE: 'v1/public/tele-sub/',
+  TELEGRAM_SUBSCRIBE: '/v1/public/telegram/subcribe/',
 
   COWIN_BASE_URL: 'https://cdn-api.co-vin.in/api',
   COWIN_DISTRICT: '/v2/admin/location/districts/',
