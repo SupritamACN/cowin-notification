b58bade0d7a94de34910db20aec94cfffdb73c2a
Supritam <supritamsadhukhan@gmail.com>
2021-05-22 16:38:40 +0000

    issue fix

--- backend/src/main/java/com/help/cowin/CowinApplication.java
+++ backend/src/main/java/com/help/cowin/CowinApplication.java
@@ -2,8 +2,10 @@ package com.help.cowin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAsync
@@ -13,5 +15,10 @@ public class CowinApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CowinApplication.class, args);
-	}
+    }
+    
+    @Bean
+    public RestTemplate getRestTemplate() {
+      return new RestTemplate();
+   }
 }
